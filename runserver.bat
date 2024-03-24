7e57bfb77d6e858d7a715497be226c5f2331a7b8
Azhar Ajmeri <azharajmeri.aaa@gmail.com>
2024-03-24 19:36:30 +0530

    bat files

---
+++ runserver.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM Install virtual environment package
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+
+REM Create virtual environment
+python -m venv venv
+
+REM Activate virtual environment
+call venv\Scripts\activate
+
+REM Install requirements
+pip install -r requirements.txt
+
+REM Display message
+echo.
+echo Virtual environment created and activated, and requirements installed.
+echo.
+
+REM Start Django development server
+python manage.py runserver
+
+REM Wait for user input before exiting
+echo.
+echo Press any key to exit...
+pause > nul
